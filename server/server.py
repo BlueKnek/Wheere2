869e346e6959dd64b34694916d5dd78ba7e7dee3
BlueKnek <blueknek@gmail.com>
2018-01-19 12:22:04 +0100

    Initial TinyDB

--- server/server.py
+++ server/server.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
+db = TinyDB('db.json')
+items = db.table('items')
 
 
 @app.route('/data.json')
 def data_json():
-    return jsonify({'hello': 'world'})
+    return jsonify({
+        'hello': 'world',
+        'items': items.all(),
+    })
+
+
+@app.route('/add-item/<string:name>')
+def set_test(name):
+    items.insert({
+        'name': name,
+    })
+    return 'Inserted '+name
+
