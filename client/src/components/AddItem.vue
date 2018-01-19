4b7b64fccc84c395019c4392438ac0c9b117171d
BlueKnek <blueknek@gmail.com>
2018-01-19 15:12:16 +0100

    Add uploading images

--- client/src/components/AddItem.vue
+++ client/src/components/AddItem.vue
@@ -3,18 +3,37 @@
     <h1>Add item</h1>
     <input v-model="name"/> <br/>
     <textarea v-model="description"/> <br/>
+    <input @change="addImageFiles($event.target.files)" type="file" accept="image/*" capture="camera"/> <br/>
     <button @click="addItem()">Add item</button> <br/>
     <span>{{status}}</span>
+
+    <h2>Images</h2>
+    <div v-for="image in images" :key="image.name">
+      <img v-if="image.dataUrl" :src="image.dataUrl"/>
+      {{image.status}}
+    </div>
   </div>
 </template>
 
 <script>
+function blobToDataUrl (blob) {
+  return new Promise((resolve, reject) => {
+    let reader = new FileReader()
+    reader.onload = event => {
+      let dataUrl = event.target.result
+      resolve(dataUrl)
+    }
+    reader.readAsDataURL(blob)
+  })
+}
+
 export default {
   data () {
     return {
       name: '',
       description: '',
       status: 'idle',
+      images: [],
     }
   },
   methods: {
@@ -31,9 +50,45 @@ export default {
         .then(r => { this.status = 'done' })
         .catch(r => { this.status = 'fail' })
     },
+    addImageFiles (files) {
+      for (let index = 0; index < files.length; index++) {
+        this.addImageFile(files[index])
+      }
+    },
+    addImageFile (file) {
+      let image = {
+        file: file,
+        dataUrl: false,
+        status: 'uploading',
+      }
+
+      blobToDataUrl(file)
+        .then(dataUrl => { image.dataUrl = dataUrl })
+
+      let formData = new FormData()
+      formData.append('image', file, file.name)
+      fetch('/api/upload-image', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(r => r.json())
+        .then(j => {
+          if (j.filename) {
+            image.filename = j.filename
+            image.status = 'success'
+          } else {
+            throw Error('Image not saved')
+          }
+        })
+        .catch(e => { image.status = 'fail' })
+      this.images.push(image)
+    },
   },
 }
 </script>
 
 <style scoped>
+img {
+  max-width: 100%;
+}
 </style>
--- server/server.py
+++ server/server.py
@@ -1,9 +1,55 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from tinydb import TinyDB, Query
+from werkzeug.utils import secure_filename
+import os
+
+UPLOADED_IMAGES = 'uploaded_images'
 
 app = Flask(__name__)
 db = TinyDB('db.json')
 items = db.table('items')
+
+
+def new_uploaded_filename(folder, filename):
+    filename = secure_filename(filename)
+    if not os.path.exists(os.path.join(folder, filename)):
+        return filename
+    else:
+        name, extension = os.path.splitext(filename)
+        for i in range(999):
+            filename_num = name + '_' + str(i).zfill(3) + extension
+            if not os.path.exists(os.path.join(folder, filename_num)):
+                return filename_num
+        raise EnvironmentError('Problem with finding free filename for ' + filename)
+
+
+def new_uploaded_image_filename(filename):
+    return new_uploaded_filename(UPLOADED_IMAGES, filename)
+
+
+def save_image_if_exists():
+    if 'image' in request.files:
+        file = request.files['image']
+        if file and file.filename:
+            print(file.filename)
+            filename = new_uploaded_image_filename(file.filename)
+            file.save(os.path.join(UPLOADED_IMAGES, filename))
+            return filename
+    return ''
+
+
+@app.route('/api/img/<string:filename>')
+def uploaded_image(filename):
+    return send_from_directory(UPLOADED_IMAGES, filename)
+
+
+@app.route('/api/upload-image', methods=['POST'])
+def upload_image():
+    filename = save_image_if_exists()
+    if filename:
+        return jsonify({'filename': filename})
+    else:
+        return jsonify({}), 500
 
 
 @app.route('/api/data.json')
@@ -24,4 +70,5 @@ def set_test():
         'description': description,
     })
     return 'Inserted '+name
+
 
