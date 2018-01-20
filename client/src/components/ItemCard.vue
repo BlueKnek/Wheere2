a2d782773c02975c8559837d4c58c599c454d12d
BlueKnek <blueknek@gmail.com>
2018-01-20 12:51:55 +0100

    ItemCard

---
+++ client/src/components/ItemCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ItemCard">
+    <Thumbnail :image="{filename: item.images[0]}"/>
+    <scope class="name">{{item.name}}</scope>
+  </div>
+</template>
+
+<script>
+import Thumbnail from './Thumbnail'
+
+export default {
+  components: {
+    Thumbnail,
+  },
+  props: ['item'],
+}
+</script>
+
+<style scoped>
+  .ItemCard {
+    --space: 1rem;
+    --space-inside: 0.25rem;
+    display: flex;
+    padding: var(--space-inside);
+    margin: var(--space);
+    border-radius: var(--space-inside);
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  }
+
+  .ItemCard > * {
+    margin: var(--space-inside);
+  }
+
+  .name {
+    font-size: 2rem;
+  }
+</style>
--- client/src/components/Root.vue
+++ client/src/components/Root.vue
@@ -2,12 +2,18 @@
   <div class="root">
     <h1>Wheere2</h1>
     <router-link to="/new-item">New item</router-link>
+    <ItemCard v-for="item in data.items" :item="item" :key="item.item_id"/>
     <div>{{data}}</div>
   </div>
 </template>
 
 <script>
+import ItemCard from './ItemCard'
+
 export default {
+  components: {
+    ItemCard,
+  },
   data () {
     fetch('/api/data.json')
       .then(r => r.json())
