2ce36047f8a7a37b9d1313cb3c860dba8f271cee
BlueKnek <blueknek@gmail.com>
2018-01-21 10:43:25 +0100

    Style - ItemCard

--- client/src/components/ItemCard.vue
+++ client/src/components/ItemCard.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div.ItemCard(@click="go")
     Thumbnail(:filename="item.images[0]")
+    div.space
     div.ItemCard-info
       span.name {{item.name}}
       Tags(:value="item.tags")
@@ -27,10 +28,8 @@ export default {
 <style scoped>
   .ItemCard {
     --space: 1rem;
-    --space-inside: 0.25rem;
     background-color: var(--background-color);
     display: flex;
-    padding: var(--space-inside);
     margin: var(--space);
     box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
     transition: all 0.5s;
@@ -41,16 +40,13 @@ export default {
     box-shadow: 0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3);
   }
 
-  .ItemCard > * {
-    margin: var(--space-inside);
-  }
-
   .ItemCard-info {
+    padding: 0.125rem 0.5rem;
     display: flex;
     flex-direction: column;
   }
 
   .name {
-    font-size: 2rem;
+    font-size: 1.5rem;
   }
 </style>
--- client/src/components/Thumbnail.vue
+++ client/src/components/Thumbnail.vue
@@ -26,6 +26,5 @@ img {
   width: 5rem;
   height: 5rem;
   object-fit: cover;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
 }
 </style>
